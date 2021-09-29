fe530097e134700e952d28fc3f7557073c4ef7a9
Nacho Cebey <n.cebey@gmail.com>
2021-09-29 18:30:44 +0200

    Adding animationto hero-name

--- src/Views/Main/Components/HeroTitle/HeroTitle.js
+++ src/Views/Main/Components/HeroTitle/HeroTitle.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
+
+const Hero = styled.div`
+  font-size: 8.9vmax;
+  line-height: 1;
+`;
 
 export default function HeroTitle() {
+  const calc = (x, y) => [-(y - window.innerHeight / 2) / 40, (x - window.innerWidth / 2) / 40, 1];
+  const trans = (x, y, s) => `perspective(800px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+  const [propss, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 450, friction: 70 } }));
+
   return (
     <div className="grid place-content-center h-screen">
-      <div className="flex flex-col items-center font-hero text-9xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-red-600 to-yellow-400">
-        <span>Nacho</span>
-        <span>Cebey</span>
-        <span>Honorato</span>
-      </div>
+      <animated.div
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: propss.xys.to(trans) }}
+      >
+        <Hero className="flex flex-col items-center font-hero text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-red-600 to-yellow-400">
+          <span>Nacho</span>
+          <span>Cebey</span>
+          <span>Honorato</span>
+        </Hero>
+      </animated.div>
     </div>
   );
 }
